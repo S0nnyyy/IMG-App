f17ce832a42a9d85c7141dd6aa974c6504804f63
theso <theso@MARTIN>
2024-04-18 01:54:10 +0200

    Přidejte soubory projektu.

---
+++ DesatyUkol/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace DesatyUkol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vložení obrázku";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 71);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Vložit obrázek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3. obrázek";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2. obrázek";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1. obrázek";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Smazání obrázku";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 71);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Smazat obrázek";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 65);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(75, 17);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "3. obrázek";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 42);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(75, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "2. obrázek";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 19);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(75, 17);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "1. obrázek";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(146, 118);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(138, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_DoubleClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(290, 118);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(128, 96);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.DoubleClick += new System.EventHandler(this.pictureBox3_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(426, 226);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Galerie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ DesatyUkol/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesatyUkol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (pictureBox1.Image != null)
+                {
+                    MessageBox.Show("Chyba: V PictureBoxu 1 je již nahrán obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    VlozitObrázekDoPictureBoxu(pictureBox1);
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                if (pictureBox2.Image != null)
+                {
+                    MessageBox.Show("Chyba: V PictureBoxu 2 je již nahrán obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    VlozitObrázekDoPictureBoxu(pictureBox2);
+                }
+            }
+            else if (radioButton3.Checked)
+            {
+                if (pictureBox3.Image != null)
+                {
+                    MessageBox.Show("Chyba: V PictureBoxu 3 je již nahrán obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    VlozitObrázekDoPictureBoxu(pictureBox3);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton6.Checked)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    MessageBox.Show("Chyba: V PictureBoxu 1 není žádný obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    pictureBox1.Image = null;
+                }
+            }
+            else if (radioButton5.Checked)
+            {
+                if (pictureBox2.Image == null)
+                {
+                    MessageBox.Show("Chyba: V PictureBoxu 2 není žádný obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    pictureBox2.Image = null;
+                }
+            }
+            else if (radioButton4.Checked)
+            {
+                if (pictureBox3.Image == null)
+                {
+                    MessageBox.Show("Chyba: V PictureBoxu 3 není žádný obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    pictureBox3.Image = null;
+                }
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+                radioButton1.Checked = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+                radioButton2.Checked = true;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+                radioButton3.Checked = true;
+        }
+        private void VlozitObrázekDoPictureBoxu(PictureBox pictureBox)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Obrázky|*.jpg;*.jpeg;*.png;";
+            openFileDialog.Title = "Vyberte obrázek";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Načíst vybraný obrázek
+                Image selectedImage = Image.FromFile(openFileDialog.FileName);
+
+                // Zobrazit obrázek v předaném PictureBoxu
+                pictureBox.Image = selectedImage;
+            }
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            ZobrazitZvetsenyObrazek(pictureBox1);
+        }
+
+        private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        {
+            ZobrazitZvetsenyObrazek(pictureBox2);
+
+        }
+
+        private void pictureBox3_DoubleClick(object sender, EventArgs e)
+        {
+            ZobrazitZvetsenyObrazek(pictureBox3);
+        }
+
+        private void ZobrazitZvetsenyObrazek(PictureBox pictureBox)
+        {
+            if (pictureBox.Image != null)
+            {
+                Form zvetsenyObrazekForm = new Form();
+                zvetsenyObrazekForm.StartPosition = FormStartPosition.CenterScreen;
+                zvetsenyObrazekForm.Size = new Size(pictureBox.Image.Width, pictureBox.Image.Height);
+                zvetsenyObrazekForm.BackgroundImage = pictureBox.Image;
+                zvetsenyObrazekForm.Text = "Zvětšený obrázek";
+                zvetsenyObrazekForm.BackgroundImageLayout = ImageLayout.Zoom;
+                zvetsenyObrazekForm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show($"Chyba: V PictureBoxu {(pictureBox.Name == "pictureBox1" ? "1" : pictureBox.Name == "pictureBox2" ? "2" : "3")} není žádný obrázek.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
